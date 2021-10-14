f2a513851f69f99323519d6bbccad19e6dd1d2de
Josh Holmer <jholmer.in@gmail.com>
2021-10-13 23:31:03 -0400

    Revert "Don't use deltaq-mode 5"
    This reverts commit c4d46a1cd0c4d53208d81dd9feb34cb63f5c9aa3.

--- src/output/video.rs
+++ src/output/video.rs
@@ -277,13 +277,14 @@ fn build_aom_args_string(
 ) -> String {
     format!(
         " --cpu-used={} --end-usage=q --cq-level={} --lag-in-frames=48 --enable-fwd-kf=1 \
-         --deltaq-mode=1 --enable-chroma-deltaq=1 --quant-b-adapt=1 --enable-qm=1 --qm-min=0 \
+         --deltaq-mode={} --enable-chroma-deltaq=1 --quant-b-adapt=1 --enable-qm=1 --qm-min=0 \
          --min-q=1 --enable-keyframe-filtering={} --arnr-strength=4 --arnr-maxframes={} \
          --sharpness=2 --enable-dnl-denoising=0 --tune=image_perceptual_quality --tile-columns={} \
          --tile-rows=0 --threads=4 --row-mt=0 --color-primaries={} --transfer-characteristics={} \
          --matrix-coefficients={} --disable-kf ",
         speed.unwrap_or(4),
         crf,
+        if is_hdr { 5 } else { 1 },
         if compat == Compat::None { 2 } else { 0 },
         if profile == Profile::Anime { 15 } else { 7 },
         if dimensions.width >= 1200 { 1 } else { 0 },
