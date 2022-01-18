dc7f769751dfcdbb969f9398c6dc7a82861bb0b0
Josh Holmer <jholmer.in@gmail.com>
2022-01-18 09:29:24 -0500

    Increase max threads for aom

--- src/output/video.rs
+++ src/output/video.rs
@@ -380,8 +380,8 @@ fn build_aom_args_string(
          --quant-b-adapt=1 --enable-qm=1 --min-q=1 --enable-keyframe-filtering=0 \
          --arnr-strength=1 --arnr-maxframes={} --sharpness=2 --enable-dnl-denoising=0 \
          --disable-trellis-quant=0 --tune=image_perceptual_quality --tile-columns={} \
-         --tile-rows={} --threads=4 --row-mt=0 --color-primaries={} --transfer-characteristics={} \
-         --matrix-coefficients={} --disable-kf --kf-max-dist=9999 ",
+         --tile-rows={} --threads=64 --row-mt=0 --color-primaries={} \
+         --transfer-characteristics={} --matrix-coefficients={} --disable-kf --kf-max-dist=9999 ",
         speed,
         crf,
         if profile == Profile::Anime {
