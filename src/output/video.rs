a70d31fa6ca6af34576ba0bb8d10b3dde5171836
Josh Holmer <jholmer.in@gmail.com>
2022-09-16 09:51:31 -0400

    Buttery males

--- src/output/video.rs
+++ src/output/video.rs
@@ -422,7 +422,7 @@ fn build_aom_args_string(
         " --cpu-used={} --cq-level={} --end-usage=q --tune-content={} --lag-in-frames=64 \
          --aq-mode=1 --deltaq-mode={} --enable-chroma-deltaq=1 --quant-b-adapt=1 --enable-qm=1 \
          --min-q=1 --arnr-strength=1 --arnr-maxframes=4 --sharpness=3 --enable-dnl-denoising=0 \
-         --disable-trellis-quant=0 --enable-dual-filter=0 --tune=image_perceptual_quality \
+         --disable-trellis-quant=0 --enable-dual-filter=0 --tune=butteraugli \
          --tile-columns={} --tile-rows={} --threads={} --row-mt=0 --color-primaries={} \
          --transfer-characteristics={} --matrix-coefficients={} -b {} --disable-kf ",
         speed,
