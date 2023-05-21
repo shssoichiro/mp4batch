d51dd2a1f57b2a75be82807c8b74ba9bc4c5752c
Josh Holmer <jholmer.in@gmail.com>
2023-05-21 17:04:08 -0400

    Tweak qm-max

--- src/output/video.rs
+++ src/output/video.rs
@@ -433,7 +433,7 @@ fn build_aom_args_string(
         " -b {} --end-usage=q --min-q=1 --lag-in-frames=64 --cpu-used={speed} --cq-level={crf} \
          --disable-kf --kf-max-dist=9999 --enable-fwd-kf=0 --sharpness=3 --row-mt=0 \
          --tile-columns={} --tile-rows={} --arnr-maxframes=15 --arnr-strength={} --tune=ssim  \
-         --enable-chroma-deltaq=1 --disable-trellis-quant=0 --enable-qm=1 --qm-min=0 --qm-max=12 \
+         --enable-chroma-deltaq=1 --disable-trellis-quant=0 --enable-qm=1 --qm-min=0 --qm-max=8 \
          --quant-b-adapt=1 --aq-mode=0 --deltaq-mode={} --tune-content=psy --color-primaries={} \
          --transfer-characteristics={} --matrix-coefficients={} --sb-size=dynamic \
          --enable-dnl-denoising=0 --threads={threads} ",
