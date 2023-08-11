f4660d7d1e42bb85deaf250447660f492db612a7
Josh Holmer <jholmer.in@gmail.com>
2023-08-11 09:24:28 -0400

    Try loosening the early exit checking

--- src/output/video.rs
+++ src/output/video.rs
@@ -181,7 +181,7 @@ pub fn create_lossless(input: &Path, dimensions: VideoDimensions) -> Result<()> 
         // We use a fuzzy frame count check because *some cursed sources*
         // report a different frame count from the number of actual decodeable frames.
         let diff = (lossless_frames as i64 - dimensions.frames as i64).unsigned_abs() as u32;
-        let allowance = dimensions.frames / 200;
+        let allowance = dimensions.frames / 100;
         if diff > allowance {
             anyhow::bail!("Incomplete lossless encode");
         }
