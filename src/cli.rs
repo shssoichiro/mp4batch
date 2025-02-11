1c2ff32ac6338a4853aca5c63356405ccc317caf
Josh Holmer <jholmer.in@gmail.com>
2025-02-11 14:46:56 -0500

    Finally update nom

--- src/cli.rs
+++ src/cli.rs
@@ -9,8 +9,8 @@ use nom::{
     character::complete::{alpha1, alphanumeric1, char, digit1},
     combinator::{opt, recognize},
     multi::separated_list1,
-    sequence::{preceded, tuple},
-    IResult,
+    sequence::preceded,
+    IResult, Parser,
 };
 
 use crate::{AudioEncoder, Profile, VideoEncoder};
@@ -72,109 +72,125 @@ pub fn parse_filters<'a>(input: &'a str, in_file: &Path) -> Vec<ParsedFilter<'a>
 }
 
 fn parse_video_encoder(input: &str) -> IResult<&str, ParsedFilter> {
-    preceded(tag("enc="), alphanumeric1)(input).map(|(input, token)| {
-        if VideoEncoder::supported_encoders().contains(&token) {
-            (input, ParsedFilter::VideoEncoder(token))
-        } else {
-            panic!("Unrecognize video encoder: {}", token);
-        }
-    })
+    preceded(tag("enc="), alphanumeric1)
+        .parse_complete(input)
+        .map(|(input, token)| {
+            if VideoEncoder::supported_encoders().contains(&token) {
+                (input, ParsedFilter::VideoEncoder(token))
+            } else {
+                panic!("Unrecognize video encoder: {}", token);
+            }
+        })
 }
 
 fn parse_quantizer(input: &str) -> IResult<&str, ParsedFilter> {
     preceded(
         alt((tag("q="), tag("qp="), tag("crf="))),
-        recognize(tuple((opt(char('-')), digit1))),
-    )(input)
+        recognize((opt(char('-')), digit1)),
+    )
+    .parse_complete(input)
     .map(|(input, token)| (input, ParsedFilter::Quantizer(token.parse().unwrap())))
 }
 
 fn parse_speed(input: &str) -> IResult<&str, ParsedFilter> {
-    preceded(alt((tag("s="), tag("speed="))), digit1)(input)
+    preceded(alt((tag("s="), tag("speed="))), digit1)
+        .parse_complete(input)
         .map(|(input, token)| (input, ParsedFilter::Speed(token.parse().unwrap())))
 }
 
 fn parse_profile(input: &str) -> IResult<&str, ParsedFilter> {
-    preceded(alt((tag("p="), tag("profile="))), alpha1)(input).map(|(input, token)| {
-        (
-            input,
-            ParsedFilter::Profile(Profile::from_str(token).unwrap()),
-        )
-    })
+    preceded(alt((tag("p="), tag("profile="))), alpha1)
+        .parse_complete(input)
+        .map(|(input, token)| {
+            (
+                input,
+                ParsedFilter::Profile(Profile::from_str(token).unwrap()),
+            )
+        })
 }
 
 fn parse_grain(input: &str) -> IResult<&str, ParsedFilter> {
-    preceded(alt((tag("g="), tag("grain="))), digit1)(input)
+    preceded(alt((tag("g="), tag("grain="))), digit1)
+        .parse_complete(input)
         .map(|(input, token)| (input, ParsedFilter::Grain(token.parse().unwrap())))
 }
 
 fn parse_compat(input: &str) -> IResult<&str, ParsedFilter> {
-    preceded(tag("compat="), digit1)(input).map(|(input, token)| {
-        (
-            input,
-            ParsedFilter::Compat(token.parse::<u8>().unwrap() > 0),
-        )
-    })
+    preceded(tag("compat="), digit1)
+        .parse_complete(input)
+        .map(|(input, token)| {
+            (
+                input,
+                ParsedFilter::Compat(token.parse::<u8>().unwrap() > 0),
+            )
+        })
 }
 
 fn parse_extension(input: &str) -> IResult<&str, ParsedFilter> {
-    preceded(tag("ext="), alphanumeric1)(input).map(|(input, token)| {
-        if token == "mp4" || token == "mkv" {
-            (input, ParsedFilter::Extension(token))
-        } else {
-            panic!("Unsupported extension: {}", token);
-        }
-    })
+    preceded(tag("ext="), alphanumeric1)
+        .parse_complete(input)
+        .map(|(input, token)| {
+            if token == "mp4" || token == "mkv" {
+                (input, ParsedFilter::Extension(token))
+            } else {
+                panic!("Unsupported extension: {}", token);
+            }
+        })
 }
 
 fn parse_bit_depth(input: &str) -> IResult<&str, ParsedFilter> {
-    preceded(tag("bd="), digit1)(input).map(|(input, token)| {
-        if token == "8" || token == "10" {
-            (input, ParsedFilter::BitDepth(token.parse().unwrap()))
-        } else {
-            panic!("Unsupported bit depth: {}", token);
-        }
-    })
+    preceded(tag("bd="), digit1)
+        .parse_complete(input)
+        .map(|(input, token)| {
+            if token == "8" || token == "10" {
+                (input, ParsedFilter::BitDepth(token.parse().unwrap()))
+            } else {
+                panic!("Unsupported bit depth: {}", token);
+            }
+        })
 }
 
 fn parse_resolution(input: &str) -> IResult<&str, ParsedFilter> {
-    preceded(tag("res="), tuple((digit1, char('x'), digit1)))(input).map(|(input, (w, _, h))| {
-        let width = w.parse::<u32>().unwrap();
-        let height = h.parse::<u32>().unwrap();
-        if width % 2 != 0 || height % 2 != 0 {
-            panic!("Resolution must be mod 2, got {}x{}", w, h);
-        }
-        if width < 64 || height < 64 {
-            panic!("Resolution must be at least 64x64, got {}x{}", w, h);
-        }
+    preceded(tag("res="), (digit1, char('x'), digit1))
+        .parse_complete(input)
+        .map(|(input, (w, _, h))| {
+            let width = w.parse::<u32>().unwrap();
+            let height = h.parse::<u32>().unwrap();
+            if width % 2 != 0 || height % 2 != 0 {
+                panic!("Resolution must be mod 2, got {}x{}", w, h);
+            }
+            if width < 64 || height < 64 {
+                panic!("Resolution must be at least 64x64, got {}x{}", w, h);
+            }
 
-        (input, ParsedFilter::Resolution { width, height })
-    })
+            (input, ParsedFilter::Resolution { width, height })
+        })
 }
 
 fn parse_audio_encoder(input: &str) -> IResult<&str, ParsedFilter> {
-    preceded(tag("aenc="), alphanumeric1)(input).map(|(input, token)| {
-        if AudioEncoder::supported_encoders().contains(&token) {
-            (input, ParsedFilter::AudioEncoder(token))
-        } else {
-            panic!("Unrecognize audio encoder: {}", token);
-        }
-    })
+    preceded(tag("aenc="), alphanumeric1)
+        .parse_complete(input)
+        .map(|(input, token)| {
+            if AudioEncoder::supported_encoders().contains(&token) {
+                (input, ParsedFilter::AudioEncoder(token))
+            } else {
+                panic!("Unrecognize audio encoder: {}", token);
+            }
+        })
 }
 
 fn parse_audio_bitrate(input: &str) -> IResult<&str, ParsedFilter> {
-    preceded(tag("ab="), digit1)(input)
+    preceded(tag("ab="), digit1)
+        .parse_complete(input)
         .map(|(input, token)| (input, ParsedFilter::AudioBitrate(token.parse().unwrap())))
 }
 
 fn parse_audio_tracks<'a>(input: &'a str, in_file: &Path) -> IResult<&'a str, ParsedFilter<'a>> {
     preceded(
         tag("at="),
-        separated_list1(
-            char('|'),
-            tuple((alphanumeric1, opt(preceded(char('-'), alpha1)))),
-        ),
-    )(input)
+        separated_list1(char('|'), (alphanumeric1, opt(preceded(char('-'), alpha1)))),
+    )
+    .parse_complete(input)
     .map(|(input, tokens)| {
         (
             input,
@@ -209,11 +225,9 @@ fn parse_audio_norm(input: &str) -> IResult<&str, ParsedFilter> {
 fn parse_subtitle_tracks<'a>(input: &'a str, in_file: &Path) -> IResult<&'a str, ParsedFilter<'a>> {
     preceded(
         tag("st="),
-        separated_list1(
-            char('|'),
-            tuple((alphanumeric1, opt(preceded(char('-'), alpha1)))),
-        ),
-    )(input)
+        separated_list1(char('|'), (alphanumeric1, opt(preceded(char('-'), alpha1)))),
+    )
+    .parse_complete(input)
     .map(|(input, tokens)| {
         (
             input,
