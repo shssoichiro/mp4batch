a9ddf144dc1693b097eaa893afbc9971c9b20278
Josh Holmer <jholmer.in@gmail.com>
2025-06-29 06:11:50 -0400

    We don't need to support mp4 output anymore

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -1,10 +1,8 @@
 use std::{
-    borrow::Cow,
     path::{Path, PathBuf},
     process::{Command, Stdio},
 };
 
-use ansi_term::Colour::Yellow;
 use anyhow::Result;
 
 use crate::{
@@ -36,223 +34,103 @@ pub fn mux_video(
     ignore_delay: bool,
     output: &Path,
 ) -> Result<()> {
-    let mut extension = output
-        .extension()
-        .expect("Video should have extension")
-        .to_string_lossy();
-
-    if extension != "mkv" && !subtitles.is_empty() {
-        eprintln!(
-            "{} {}",
-            Yellow.bold().paint("[Warning]"),
-            Yellow.paint("Subtitles present, forcing mkv"),
-        );
-        extension = Cow::Borrowed("mkv");
-    }
-    if extension == "mkv" {
-        let mut track_order = vec!["0:0".to_string()];
-        let mut inputs_read = 1;
-        let mut command = Command::new("mkvmerge");
-        command
-            .arg("--output")
-            .arg(output)
-            .arg("--no-audio")
-            .arg("--no-subtitles")
-            .arg("--no-attachments")
-            .arg("--no-chapters")
-            .arg("--language")
-            .arg("0:en")
-            .arg("(")
-            .arg(video)
-            .arg(")");
-        if !audios.is_empty() {
-            for audio in audios {
-                let audio_delay = if ignore_delay || audio.2 == AudioEncoder::Copy {
-                    // If we're copying, mkvtoolnix copies the sync automatically.
-                    0
-                } else {
-                    // If we're reencoding the audio, then we need to manually apply the sync.
-                    // Note that mediainfo can give unparseable and wrong results for some formats
-                    // like PCM, so we just assume 0 for those.
-                    get_audio_delay_ms(
-                        &match audio.1.source {
-                            TrackSource::FromVideo(_) => find_source_file(input),
-                            TrackSource::External(ref path) => path.clone(),
-                        },
-                        match audio.1.source {
-                            TrackSource::FromVideo(id) => id as usize,
-                            TrackSource::External(_) => 0,
-                        },
-                    )
-                    .unwrap_or(0)
-                };
-
-                command
-                    .arg("--audio-tracks")
-                    .arg("0")
-                    .arg("--no-video")
-                    .arg("--no-subtitles")
-                    .arg("--no-attachments")
-                    .arg("--no-chapters");
-                if let Some(timestamps) = timestamps {
-                    command
-                        .arg("--timestamps")
-                        .arg(format!("0:{}", timestamps.to_string_lossy()));
-                }
-                if audio_delay != 0 {
-                    command.arg("--sync").arg(format!("{}:{}", 0, audio_delay));
-                }
-                command
-                    .arg("--language")
-                    .arg("0:und")
-                    .arg("--track-enabled-flag")
-                    .arg(format!("0:{}", if audio.1.enabled { "yes" } else { "no" }))
-                    .arg("--forced-display-flag")
-                    .arg(format!("0:{}", if audio.1.forced { "yes" } else { "no" }))
-                    .arg("(")
-                    .arg(&audio.0)
-                    .arg(")");
-                track_order.push(format!("{}:0", inputs_read));
-                inputs_read += 1;
-            }
-        }
-        if !subtitles.is_empty() {
-            for subtitle in subtitles {
-                command
-                    .arg("--no-video")
-                    .arg("--no-audio")
-                    .arg("--no-attachments")
-                    .arg("--language")
-                    .arg("0:en")
-                    .arg("--sub-charset")
-                    .arg("0:UTF-8")
-                    .arg("--track-enabled-flag")
-                    .arg(format!("0:{}", if subtitle.1 { "yes" } else { "no" }))
-                    .arg("--forced-display-flag")
-                    .arg(format!("0:{}", if subtitle.2 { "yes" } else { "no" }))
-                    .arg("(")
-                    .arg(&subtitle.0)
-                    .arg(")");
-                track_order.push(format!("{}:0", inputs_read));
-                inputs_read += 1;
-            }
-        }
-        if copy_fonts {
-            eprintln!("WARNING: copy fonts not currently implemented for mkv");
-        }
-        command.arg("--track-order").arg(track_order.join(","));
-
-        let status = command.status()?;
-        if status.success() {
-            Ok(())
-        } else {
-            anyhow::bail!("Failed to mux video");
-        }
-    } else {
-        let mut command = Command::new("ffmpeg");
-        command
-            .arg("-hide_banner")
-            .arg("-loglevel")
-            .arg("level+error")
-            .arg("-stats")
-            .arg("-i")
-            .arg(video);
+    let mut track_order = vec!["0:0".to_string()];
+    let mut inputs_read = 1;
+    let mut command = Command::new("mkvmerge");
+    command
+        .arg("--output")
+        .arg(output)
+        .arg("--no-audio")
+        .arg("--no-subtitles")
+        .arg("--no-attachments")
+        .arg("--no-chapters")
+        .arg("--language")
+        .arg("0:en")
+        .arg("(")
+        .arg(video)
+        .arg(")");
+    if !audios.is_empty() {
         for audio in audios {
-            command.arg("-i").arg(&audio.0);
-        }
-        for subtitle in subtitles {
-            command.arg("-i").arg(&subtitle.0);
-        }
-        if copy_fonts {
-            command.arg("-i").arg(input);
-        }
-        command
-            .arg("-vcodec")
-            .arg("copy")
-            .arg("-acodec")
-            .arg("copy");
-        if !subtitles.is_empty() {
-            command.arg("-c:s").arg("copy");
-        }
-        command.arg("-map").arg("0:v:0");
-        let mut i = 1;
-        for (j, audio) in audios.iter().enumerate() {
-            command.arg("-map").arg(format!("{}:a:0", i));
-            if audio.1.forced {
-                command.arg(format!("-disposition:a:{}", j)).arg("forced");
-            } else if audio.1.enabled {
-                command.arg(format!("-disposition:a:{}", j)).arg("default");
-            }
-            i += 1;
-        }
-        for (j, subtitle) in subtitles.iter().enumerate() {
-            command.arg("-map").arg(format!("{}:s:0", i));
-            if subtitle.2 {
-                command.arg(format!("-disposition:s:{}", j)).arg("forced");
-            } else if subtitle.1 {
-                command.arg(format!("-disposition:s:{}", j)).arg("default");
-            }
-            i += 1;
-        }
-        if copy_fonts {
-            command
-                .arg("-map")
-                .arg(format!("{}:t?", 1 + audios.len() + subtitles.len()));
-        } else {
-            let fonts_dir = input
-                .parent()
-                .expect("File should have parent dir")
-                .join("fonts");
-            if fonts_dir.is_dir() {
-                let fonts = fonts_dir
-                    .read_dir()
-                    .expect("Unable to read directory contents");
-                let mut i = 0;
-                for font in fonts {
-                    let font = font.expect("Invalid directory entry");
-                    let mimetype = match font
-                        .path()
-                        .extension()
-                        .and_then(|ext| ext.to_str())
-                        .map_or_else(String::new, |ext| ext.to_lowercase())
-                        .as_str()
-                    {
-                        "ttf" => "font/ttf",
-                        "otf" => "font/otf",
-                        "eot" => "application/vnd.ms-fontobject",
-                        "woff" => "font/woff",
-                        "woff2" => "font/woff2",
-                        _ => {
-                            eprintln!(
-                                "{} {}",
-                                Yellow.bold().paint("[Warning]"),
-                                Yellow.paint(format!(
-                                    "Attachment with unrecognized extension skipped: {}",
-                                    font.path().to_string_lossy()
-                                )),
-                            );
-                            continue;
-                        }
-                    };
-                    command
-                        .arg("-attach")
-                        .arg(font.path())
-                        .arg(format!("-metadata:s:t:{}", i))
-                        .arg(format!("mimetype={}", mimetype));
-                    i += 1;
-                }
-            }
-        }
-        if extension == "mp4" {
-            command.arg("-movflags").arg("+faststart");
-        }
+            let audio_delay = if ignore_delay || audio.2 == AudioEncoder::Copy {
+                // If we're copying, mkvtoolnix copies the sync automatically.
+                0
+            } else {
+                // If we're reencoding the audio, then we need to manually apply the sync.
+                // Note that mediainfo can give unparseable and wrong results for some formats
+                // like PCM, so we just assume 0 for those.
+                get_audio_delay_ms(
+                    &match audio.1.source {
+                        TrackSource::FromVideo(_) => find_source_file(input),
+                        TrackSource::External(ref path) => path.clone(),
+                    },
+                    match audio.1.source {
+                        TrackSource::FromVideo(id) => id as usize,
+                        TrackSource::External(_) => 0,
+                    },
+                )
+                .unwrap_or(0)
+            };
 
-        let status = command.arg(output).status()?;
-        if status.success() {
-            Ok(())
-        } else {
-            anyhow::bail!("Failed to mux video");
+            command
+                .arg("--audio-tracks")
+                .arg("0")
+                .arg("--no-video")
+                .arg("--no-subtitles")
+                .arg("--no-attachments")
+                .arg("--no-chapters");
+            if let Some(timestamps) = timestamps {
+                command
+                    .arg("--timestamps")
+                    .arg(format!("0:{}", timestamps.to_string_lossy()));
+            }
+            if audio_delay != 0 {
+                command.arg("--sync").arg(format!("{}:{}", 0, audio_delay));
+            }
+            command
+                .arg("--language")
+                .arg("0:und")
+                .arg("--track-enabled-flag")
+                .arg(format!("0:{}", if audio.1.enabled { "yes" } else { "no" }))
+                .arg("--forced-display-flag")
+                .arg(format!("0:{}", if audio.1.forced { "yes" } else { "no" }))
+                .arg("(")
+                .arg(&audio.0)
+                .arg(")");
+            track_order.push(format!("{}:0", inputs_read));
+            inputs_read += 1;
         }
+    }
+    if !subtitles.is_empty() {
+        for subtitle in subtitles {
+            command
+                .arg("--no-video")
+                .arg("--no-audio")
+                .arg("--no-attachments")
+                .arg("--language")
+                .arg("0:en")
+                .arg("--sub-charset")
+                .arg("0:UTF-8")
+                .arg("--track-enabled-flag")
+                .arg(format!("0:{}", if subtitle.1 { "yes" } else { "no" }))
+                .arg("--forced-display-flag")
+                .arg(format!("0:{}", if subtitle.2 { "yes" } else { "no" }))
+                .arg("(")
+                .arg(&subtitle.0)
+                .arg(")");
+            track_order.push(format!("{}:0", inputs_read));
+            inputs_read += 1;
+        }
+    }
+    if copy_fonts {
+        eprintln!("WARNING: copy fonts not currently implemented for mkv");
+    }
+    command.arg("--track-order").arg(track_order.join(","));
+
+    let status = command.status()?;
+    if status.success() {
+        Ok(())
+    } else {
+        anyhow::bail!("Failed to mux video");
     }
 }
 
